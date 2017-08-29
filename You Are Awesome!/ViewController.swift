9e8169ff47f30566822ebbe3a6a97008670399ed
Vlad Chilom <vchilom6@gmail.com>
2017-08-29 15:31:05 -0400

    Section 2.2

--- You Are Awesome!/ViewController.swift
+++ You Are Awesome!/ViewController.swift
@@ -9,17 +9,20 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+  
+  @IBOutlet weak var messageLabel: UILabel!
+  
+  @IBAction func messageButtonPressed(_ sender: UIButton) {
+    messageLabel.text = "You Are Awesome!"
+  }
+  
+  @IBAction func anotherMessageButtonPressed(_ sender: UIButton) {
+    messageLabel.text = "You Are Great!"
+  }
+  
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
   }
 
-  override func didReceiveMemoryWarning() {
-    super.didReceiveMemoryWarning()
-    // Dispose of any resources that can be recreated.
-  }
-
-
 }
-
