ffd21a2ca544bb76f4a5d5157060a44fa94d11f6
Vlad Chilom <vchilom6@gmail.com>
2017-09-05 12:30:49 -0400

    2.6 (last commit had 2.5 also)

--- You Are Awesome!/ViewController.swift
+++ You Are Awesome!/ViewController.swift
@@ -17,12 +17,17 @@ class ViewController: UIViewController {
   }
   
   @IBAction func messageButtonPressed(_ sender: UIButton) {
-    if messageLabel.text == "You Are Awesome!" {
-      messageLabel.text = "You Are Great!"
-    } else if messageLabel.text == "You Are Great!" {
-      messageLabel.text = "You Are Amazing!"
+    
+    let message1 = "You Are Fantastic!!!!"
+    let message2 = "You Are Great!"
+    let message3 = "You Are Amazing!"
+    
+    if messageLabel.text == message1 {
+      messageLabel.text = message2
+    } else if messageLabel.text == message2 {
+      messageLabel.text = message3
     } else {
-      messageLabel.text = "You Are Awesome!"
+      messageLabel.text = message1
     }
   }
 
